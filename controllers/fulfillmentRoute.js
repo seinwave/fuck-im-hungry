3ea0ff3468796a2fb8b89205fd9e287d4584eba1
Matt Seidholz <mseidholz@gmail.com>
2020-07-10 08:52:47 -0400

    fulfillment routes on heroku

---
+++ controllers/fulfillmentRoute.js
@@ -0,0 +1,29 @@
+const { WebhookClient } = require('dialogflow-fulfillment');
+const mongoose = require('mongoose');
+const Event = mongoose.model('events');
+
+const fulfillment = async (req, res) => {
+    const agent = new WebhookClient({ request: req, response: res });
+
+    function snoopy(agent) {
+        console.log('You figured it out, borpo!', agent.parameters);
+
+        // Event.findOne({'event': agent.parameters.event[0]})
+
+        agent.add("The Contract is Sealed")
+    }
+
+    function event(agent) {
+        console.log('')
+    }
+
+    let intentMap = new Map();
+    intentMap.set('snoopy', snoopy);
+    intentMap.set('event', event)
+}
+
+
+
+module.exports = {
+    fulfillment 
+}
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const config =  require("./config/keys.js")
 
 
+
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, { useNewUrlParser: true});
 require('./models/Events');
@@ -21,18 +22,23 @@ app.use( (req, res, next) => {
     next();
 })
 
-const tq = require('./controllers/text-query')
 
-const PORT = process.env.PORT || 3001;
+const fr = require('./controllers/fulfillmentRoute')
+const tq = require('./controllers/text-query')
 
 app.get('/', (req,res) => {
     res.send({"Borpis": "Alive"});
+})
+
+app.post('/', (req, res) => {
+    fr.fulfillment(req,res)
 })
 
 app.post('/api/df_text_query', (req,res) => {
     tq.textQuery(req,res);
 })
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`live and listening on port ${PORT}.`)
 })
