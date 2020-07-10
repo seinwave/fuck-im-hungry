03c9fd58b8e8e6057ac898fec00e16ccbe0eacd6
Matt Seidholz <mseidholz@gmail.com>
2020-07-10 07:10:16 -0400

    same shit

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
-    dialogFlowSessionID: process.env.DF_Session_ID,
+    dialogFlowSessionID: process.env.DF_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DF_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
     googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
