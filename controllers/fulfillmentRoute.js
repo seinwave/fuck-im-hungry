01dbfb5a9d6c12c2079b33138bb856dc2f6b0b9f
Matt Seidholz <mseidholz@gmail.com>
2020-07-12 09:33:57 -0400

    getting number from user query

--- controllers/fulfillmentRoute.js
+++ controllers/fulfillmentRoute.js
@@ -1,17 +1,23 @@
 const { WebhookClient } = require('dialogflow-fulfillment');
 const mongoose = require('mongoose');
 const Craving = mongoose.model('cravings')
+const nlp = require('compromise');
+nlp.extend(requre('numbers'));
 
 const fulfillment = async (req, res) => {
     const agent = new WebhookClient({ request: req, response: res });
 
     function cravings(agent){
-        console.log("This is working", agent.query )
-        const craving = new Craving({ craving: agent.parameters.degree, name: ''})
+
+        let doc = nlp(agent.query);
+        doc = doc.numbers().toNumber();
+
+        console.log("This is working", doc )
+        const craving = new Craving({ craving: agent.parameters.degree, name: '', cravingBefore: '', cravingAfter: '', intervention: ''})
         craving.name = craving._id
         craving.save()
         agent.context.set('awaiting_readiness', 3)
-        agent.add("Thanks a lot fuckboi");
+        agent.add(agent.consoleMessages[0].text); // trying this shit out
         agent.add("Now let's get to work. We can try a few different techniques to prevent a bingefest. They each take about 15 minutes. Do you have time for that right now?")
     }
 
