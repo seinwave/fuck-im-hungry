c15a47075504af343ba1426c62c91684b47363ed
Matt Seidholz <mseidholz@gmail.com>
2020-07-12 06:54:45 -0400

    making mongoos docs retrievable

--- controllers/fulfillmentRoute.js
+++ controllers/fulfillmentRoute.js
@@ -5,14 +5,8 @@ const Craving = mongoose.model('cravings')
 const fulfillment = async (req, res) => {
     const agent = new WebhookClient({ request: req, response: res });
 
-    function snoopy(agent) {
-        console.log('You figured it out, borpo!', agent.parameters);
-
-        agent.add("The Contract is Sealed")
-    }
-
     function cravings(agent){
-        console.log("This is working", agent)
+        console.log("This is working", agent.parameters)
         const craving = new Craving({ craving: agent.parameters.degree, name: ''})
         craving.name = craving._id
         craving.save()
@@ -22,7 +16,6 @@ const fulfillment = async (req, res) => {
     }
 
     let intentMap = new Map();
-    intentMap.set('snoopy', snoopy);
     intentMap.set('craving-moderate', cravings);
     intentMap.set('craving-extreme', cravings);
     intentMap.set('craving-mild', cravings);
